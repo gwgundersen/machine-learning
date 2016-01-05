099b86ef9fca211243273b82b19c5ec5644f5a5b
gwg <ggundersen@gmail.com>
2016-01-04 23:02:14 -0500

    Comments

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -3,31 +3,45 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 %   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
 %   taking num_iters gradient steps with learning rate alpha
 
-% Initialize some useful values
-m = length(y); % number of training examples
+% number of training examples
+m = length(y);
+
+% J_history is useful because, in principal, we should see J(theta) being
+% minimized over iterations.
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
-
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
-    %
-    
+        
+    % vector of predictions.
     hyp = X * theta;
+
+    % difference between the predicted and actual values.
     err = hyp - y;
-    % The change in theta
-    gradient = alpha * (1/m) * (transpose(X) * err);
-    theta = theta - gradient;
+
+    % In gradient descent with two parameters, the gradient is a 2-vector
+    % whose components are the two partial derivatives of theta. We can
+    % think of theta as an (x,y) coordinate, while J(theta) is the
+    % z-coordinate. Taking the partial derivative of each component gives
+    % us a vector that points in the direction of greatest/least slope.
+    %
+    % For a mathematical treatment, see:
+    % https://en.wikipedia.org/wiki/Gradient
+    % http://math.stackexchange.com/a/189792/159872.
+    gradient = (1/m) * (X' * err);
+
+    % We update theta with gradient. If the gradient is positive, theta
+    % becomes smaller, i.e. we're going the wrong way. If the gradient is
+    % negative, theta becomes bigger, i.e. keep going towards the local
+    % minimum.
+    %
+    % Multiply by alpha to moderate the step.
+    theta = theta - alpha * gradient;
 
     % ============================================================
 
     % Save the cost J in every iteration 
-    J_history(iter) = computeCost(X, y, theta);
-
+    J = computeCost(X, y, theta);
+    J_history(iter) = J;
 end
 
 end
