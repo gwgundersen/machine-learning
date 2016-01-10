9394fec3f99447af1158245501d1dc1f60072fe2
gwg <ggundersen@gmail.com>
2016-01-10 17:12:25 -0500

    Plot data function

--- ex2/plotData.m
+++ ex2/plotData.m
@@ -6,23 +6,21 @@ function plotData(X, y)
 % Create New Figure
 figure; hold on;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Plot the positive and negative examples on a
-%               2D plot, using the option 'k+' for the positive
-%               examples and 'ko' for the negative examples.
-%
+% `find()` returns a vector of indices of each nonzero element. In this
+% case, 0 is false.
+pos = find(y == 1);
+neg = find(y == 0);
 
+% Select (x,y) coordinates for pos / neg data points.
+pos_x_coords = X(pos);
+pos_y_coords = X(pos, 2);
 
+neg_x_coords = X(neg);
+neg_y_coords = X(neg, 2);
 
-
-
-
-
-
-
-% =========================================================================
-
-
+% Plot coordinates
+plot(pos_x_coords, pos_y_coords, 'k+', 'LineWidth', 2, 'MarkerSize', 7);
+plot(neg_x_coords, neg_y_coords, 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
 
 hold off;
 
