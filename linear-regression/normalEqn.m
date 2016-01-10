279a403b372ac263623b4084a56183ffdb42f0d0
gwg <ggundersen@gmail.com>
2016-01-10 17:24:29 -0500

    Removed cumbersome auto-generated comments

--- linear-regression/normalEqn.m
+++ linear-regression/normalEqn.m
@@ -3,15 +3,6 @@ function [theta] = normalEqn(X, y)
 %   NORMALEQN(X,y) computes the closed-form solution to linear 
 %   regression using the normal equations.
 
-theta = zeros(size(X, 2), 1);
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the code to compute the closed form solution
-%               to linear regression and put the result in theta.
-%
-
 theta = (inv(X'*X)*X')*y;
-
-% ============================================================
 
 end
