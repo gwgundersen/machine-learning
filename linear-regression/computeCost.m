78a71b69d82f0db394fc2f3604e2aff24669236e
gwg <ggundersen@gmail.com>
2016-01-10 18:32:02 -0500

    Updated comment and cleaned up code

--- linear-regression/computeCost.m
+++ linear-regression/computeCost.m
@@ -3,15 +3,8 @@ function J = computeCost(X, y, theta)
 %   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
 %   parameter for linear regression to fit the data points in X and y
 
-% Initialize some useful values
-m = length(y); % number of training examples
-
-% You need to return the following variables correctly 
-J = 0;
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta
-%               You should set J to the cost.
+% Number of training examples.
+m = length(y); 
 
 % hyp is a vector of predictions.
 hyp = X * theta;
@@ -24,7 +17,7 @@ err_sqr = err.^2;
 
 % The cost is, roughly speaking, the average of all the errors. While not
 % explicitly stated, I believe we divide by 2*m rather than just m because
-% the two cancels out when taking the partial derivative of the cost
+% the 2 cancels out when taking the partial derivative of the cost
 % function.
 %
 % You should expect to see a cost of 32.07.
