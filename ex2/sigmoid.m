7c0f0dff5ca3d164f5f0aea3de527e20fbf4181a
gwg <ggundersen@gmail.com>
2016-01-10 17:20:12 -0500

    Correct implementation of sigmoid function

--- ex2/sigmoid.m
+++ ex2/sigmoid.m
@@ -2,17 +2,10 @@ function g = sigmoid(z)
 %SIGMOID Compute sigmoid functoon
 %   J = SIGMOID(z) computes the sigmoid of z.
 
-% You need to return the following variables correctly 
-g = zeros(size(z));
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
-%               vector or scalar).
-
-
-
-
-
-% =============================================================
+% See https://en.wikipedia.org/wiki/Sigmoid_function.
+%
+% `gdivide` performs element-wise division, to ensure this function works
+% for matrices and vectors.
+g = gdivide(1, 1 + exp(-z));
 
 end
