665424bfa8b151105a877dc547a3589fb363af9b
gwg <ggundersen@gmail.com>
2016-01-13 08:39:58 -0500

    Logistic regression with regularization

--- logistic-regression/costFunctionReg.m
+++ logistic-regression/costFunctionReg.m
@@ -4,24 +4,19 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %   theta as the parameter for regularized logistic regression and the
 %   gradient of the cost w.r.t. to the parameters. 
 
-% Initialize some useful values
-m = length(y); % number of training examples
+% number of training examples
+m = length(y);
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+hyp = sigmoid(X * theta);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
+% Regularization term. Importantly, we do not regularize the first
+% component, theta(1).
+reg_term = (lambda / (2*m)) * sum(theta(2:end).^2);
+J = (sum((-y .* log(hyp)) - ((1-y) .* log(1 - hyp))) / m) + reg_term;
 
-
-
-
-
-
-% =============================================================
+reg_term = (lambda / m) * theta(2:end);
+% The right-hand expression concatenates 0, for theta(1), with the
+% regularization term, which is an (n-1)-vector.
+grad = ((X' * (hyp - y))/m) + [0; reg_term];
 
 end
