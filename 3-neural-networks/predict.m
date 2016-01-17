c417f7e46b1d684493cef1afbe94f6196f01e7b6
gwg <ggundersen@gmail.com>
2016-01-17 18:35:02 -0500

    Predictions using a forward propagation with neural network

--- 3-neural-networks/predict.m
+++ 3-neural-networks/predict.m
@@ -3,33 +3,22 @@ function p = predict(Theta1, Theta2, X)
 %   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
 %   trained weights of a neural network (Theta1, Theta2)
 
-% Useful values
+% Number of training examples
 m = size(X, 1);
-num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
-%               vector containing labels between 1 to num_labels.
-%
-% Hint: The max function might come in useful. In particular, the max
-%       function can also return the index of the max element, for more
-%       information see 'help max'. If your examples are in rows, then, you
-%       can use max(A, [], 2) to obtain the max for each row.
-%
+% Generate a2, the learn input parameters for the next layer.
+a2 = sigmoid(X * Theta1');
 
+% Add ones to the a2 data matrix.
+a2 = [ones(m, 1) a2];
 
+% All predictions, an (m x b) matrix, where b is the number of parameters
+% in Theta2, the middle layer.
+a2 = sigmoid(a2 * Theta2');
 
-
-
-
-
-
-
-% =========================================================================
-
+[discard, p] = max(a2, [], 2);
 
 end
