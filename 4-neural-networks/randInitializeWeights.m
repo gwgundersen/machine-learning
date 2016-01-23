a3e1324320697d7aaaddf6ba644c283f69694df7
gwg <ggundersen@gmail.com>
2016-01-23 16:19:54 -0500

    Random initialization of weights

--- 4-neural-networks/randInitializeWeights.m
+++ 4-neural-networks/randInitializeWeights.m
@@ -9,24 +9,7 @@ function W = randInitializeWeights(L_in, L_out)
 %   the column row of W handles the "bias" terms
 %
 
-% You need to return the following variables correctly 
-W = zeros(L_out, 1 + L_in);
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Initialize W randomly so that we break the symmetry while
-%               training the neural network.
-%
-% Note: The first row of W corresponds to the parameters for the bias units
-%
-
-
-
-
-
-
-
-
-
-% =========================================================================
+EPS_INIT = 0.12;
+W = rand(L_out, 1 + L_in) * 2 * EPS_INIT - EPS_INIT;
 
 end
