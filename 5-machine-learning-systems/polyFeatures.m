f369620c73362fc9ec8257fe8149a55318481d73
gwg <ggundersen@gmail.com>
2016-01-30 15:37:14 -0500

    Cleaned up comments

--- 5-machine-learning-systems/polyFeatures.m
+++ 5-machine-learning-systems/polyFeatures.m
@@ -5,14 +5,7 @@ function [X_poly] = polyFeatures(X, p)
 %   X_poly(i, :) = [X(i) X(i).^2 X(i).^3 ...  X(i).^p];
 %
 
-% You need to return the following variables correctly.
 X_poly = zeros(numel(X), p);
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Given a vector X, return a matrix X_poly where the p-th 
-%               column of X contains the values of X to the p-th power.
-%
-% 
 
 for i = 1:p
     X_poly(:,i) = X.^i; 
