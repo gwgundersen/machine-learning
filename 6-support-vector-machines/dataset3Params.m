4d13250e0426152bb7cb16c42133413cfc8070be
gwg <ggundersen@gmail.com>
2016-02-14 15:30:47 -0500

    Parameters optimization by testing C and sigma for multiple values

--- 6-support-vector-machines/dataset3Params.m
+++ 6-support-vector-machines/dataset3Params.m
@@ -5,30 +5,30 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
 %   sigma. You should complete this function to return the optimal C and 
 %   sigma based on a cross-validation set.
-%
 
-% You need to return the following variables correctly.
+
+vals = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+% 64 = 8*8 = length(vals) * length(vals)
+min_error = Inf;
 C = 1;
 sigma = 0.3;
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the optimal C and sigma
-%               learning parameters found using the cross validation set.
-%               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
-%                   predictions = svmPredict(model, Xval);
-%               will return the predictions on the cross validation set.
-%
-%  Note: You can compute the prediction error using 
-%        mean(double(predictions ~= yval))
-%
-
-
-
-
-
-
-
-% =========================================================================
+for i = 1:length(vals)
+    for j = 1:length(vals)
+        sigma_ = vals(i);
+        C_ = vals(j);
+        x1 = X(:,1);
+        x2 = X(:,2);
+        model = svmTrain(X, y, C_, @(x1, x2) gaussianKernel(x1, x2, sigma_)); 
+        predictions = svmPredict(model, Xval);
+        
+        % Compute prediction error and check if it is the lowest so far.
+        error = mean(double(predictions ~= yval));
+        if error < min_error
+            min_error = error;
+            C = C_;
+            sigma = sigma_;
+        end
+    end
+end
 
 end
