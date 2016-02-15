3b179198792b29506793ca35047fc21c6702bd38
gwg <ggundersen@gmail.com>
2016-02-15 16:45:49 -0500

    Completed process email

--- 6-support-vector-machines/processEmail.m
+++ 6-support-vector-machines/processEmail.m
@@ -77,37 +77,14 @@ while ~isempty(email_contents)
 
     % Look up the word in the dictionary and add to word_indices if
     % found
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Fill in this function to add the index of str to
-    %               word_indices if it is in the vocabulary. At this point
-    %               of the code, you have a stemmed word from the email in
-    %               the variable str. You should look up str in the
-    %               vocabulary list (vocabList). If a match exists, you
-    %               should add the index of the word to the word_indices
-    %               vector. Concretely, if str = 'action', then you should
-    %               look up the vocabulary list to find where in vocabList
-    %               'action' appears. For example, if vocabList{18} =
-    %               'action', then, you should add 18 to the word_indices 
-    %               vector (e.g., word_indices = [word_indices ; 18]; ).
-    % 
-    % Note: vocabList{idx} returns a the word with index idx in the
-    %       vocabulary list.
-    % 
-    % Note: You can use strcmp(str1, str2) to compare two strings (str1 and
-    %       str2). It will return 1 only if the two strings are equivalent.
-    %
+    % ============================ gwg ============================
 
-
-
-
-
-
-
-
-
+    idx = find(strcmp(str, vocabList));
+    if ~isempty(idx)
+        word_indices = [word_indices; idx];
+    end
 
     % =============================================================
-
 
     % Print to screen, ensuring that the output lines are not too long
     if (l + length(str) + 1) > 78
