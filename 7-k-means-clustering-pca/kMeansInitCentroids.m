862f151492b1aaa19a05874d95709a81379ea26f
gwg <ggundersen@gmail.com>
2016-02-21 12:07:14 -0500

    Random initialization of centroid locations

--- 7-k-means-clustering-pca/kMeansInitCentroids.m
+++ 7-k-means-clustering-pca/kMeansInitCentroids.m
@@ -5,22 +5,11 @@ function centroids = kMeansInitCentroids(X, K)
 %   used with the K-Means on the dataset X
 %
 
-% You should return this values correctly
-centroids = zeros(K, size(X, 2));
+% Randomly reorder indices of example
+randidx = randperm(size(X, 1));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: You should set centroids to randomly chosen examples from
-%               the dataset X
-%
-
-
-
-
-
-
-
-
-% =============================================================
+% Take the first K examples as centroids
+centroids = X(randidx(1:K), :);
 
 end
 
