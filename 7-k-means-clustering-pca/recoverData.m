8b19fb69a377377c74e7e8c317b0b0c9217aecb7
gwg <ggundersen@gmail.com>
2016-02-21 14:54:01 -0500

    Data recovery

--- 7-k-means-clustering-pca/recoverData.m
+++ 7-k-means-clustering-pca/recoverData.m
@@ -6,23 +6,17 @@ function X_rec = recoverData(Z, U, K)
 %   approximate reconstruction in X_rec.
 %
 
-% You need to return the following variables correctly.
-X_rec = zeros(size(Z, 1), size(U, 1));
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the approximation of the data by projecting back
-%               onto the original space using the top K eigenvectors in U.
+% Compute the approximation of the data by projecting back onto the
+% original space using the top K eigenvectors in U. For the i-th example 
+% Z(i,:), the (approximate) recovered data for dimension j is given as 
+% follows:
 %
-%               For the i-th example Z(i,:), the (approximate)
-%               recovered data for dimension j is given as follows:
-%                    v = Z(i, :)';
-%                    recovered_j = v' * U(j, 1:K)';
+%     v = Z(i, :)';
+%     recovered_j = v' * U(j, 1:K)';
 %
-%               Notice that U(j, 1:K) is a row vector.
-%               
+% Notice that U(j, 1:K) is a row vector.
 
-
-
-% =============================================================
+U_reduce = U(:,1:K);
+X_rec = Z * U_reduce';
 
 end
