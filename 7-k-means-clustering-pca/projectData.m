2f7d23314dd0b2f88ae73edda4be1c81b6263cde
gwg <ggundersen@gmail.com>
2016-02-21 14:46:34 -0500

    Projecting data onto eigenvectors

--- 7-k-means-clustering-pca/projectData.m
+++ 7-k-means-clustering-pca/projectData.m
@@ -6,21 +6,13 @@ function Z = projectData(X, U, K)
 %   the first K columns of U. It returns the projected examples in Z.
 %
 
-% You need to return the following variables correctly.
-Z = zeros(size(X, 1), K);
+% Compute the projection of the data using only the top K eigenvectors in U
+% (first K columns). For the i-th example X(i,:), the projection on to the 
+% k-th eigenvector is given as follows:
+%     x = X(i, :)';
+%     projection_k = x' * U(:, k);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the projection of the data using only the top K 
-%               eigenvectors in U (first K columns). 
-%               For the i-th example X(i,:), the projection on to the k-th 
-%               eigenvector is given as follows:
-%                    x = X(i, :)';
-%                    projection_k = x' * U(:, k);
-%
-
-
-
-
-% =============================================================
+U_reduce = U(:,1:K);
+Z = X * U_reduce;
 
 end
