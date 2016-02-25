04d405033ea0d937b1cf2da437bac09baba2a57f
gwg <ggundersen@gmail.com>
2016-02-25 08:29:13 -0500

    Comments describing algorithm

--- 8-anomaly-detection-recommender-system/selectThreshold.m
+++ 8-anomaly-detection-recommender-system/selectThreshold.m
@@ -13,25 +13,19 @@ F1 = 0;
 stepsize = (max(pval) - min(pval)) / 1000;
 for epsilon = min(pval):stepsize:max(pval)
     
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Compute the F1 score of choosing epsilon as the
-    %               threshold and place the value in F1. The code at the
-    %               end of the loop will compare the F1 score for this
-    %               choice of epsilon and set it to be the best epsilon if
-    %               it is better than the current choice of epsilon.
-    %               
-    % Note: You can use predictions = (pval < epsilon) to get a binary vector
-    %       of 0's and 1's of the outlier predictions
-
     cvPredictions = pval < epsilon;
     
+    % true and false positives; false negative
     tp = sum(cvPredictions == 1 & yval == 1);
     fp = sum(cvPredictions == 1 & yval == 0);
     fn = sum(cvPredictions == 0 & yval == 1);
     
+    % F1 score, which is a balanced metric for how well both the precision
+    % and recall did. Note that if precision or recall is very low or 0,
+    % the F1 score will be (cf. with just taking an average of the two
+    % metrics).
     precision = tp / (tp + fp);
     recall = tp / (tp + fn);
-    
     F1 = (2 * precision * recall) / (precision + recall);
     
     if F1 > bestF1
